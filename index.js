d5fdd2a3c0e268d85e384a357d2a57721edbd9e6
hbull5 <harcourtbullv@gmail.com>
2020-06-01 18:47:00 -0400

    added SQL-Scripts folder, database still needs some love

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql");
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // create connection object
 const connection = mysql.createConnection({
@@ -14,4 +15,27 @@ const connection = mysql.createConnection({
 connection.connect((err) => {
 	if (err) throw err;
 	console.log("DB connection established...");
+});
+
+// get all assignments
+app.get("/", (req, res) => {});
+
+// get specific assignments
+app.get("/:id", (req, res) => {});
+
+// insert new assignment
+app.post("/assignment/");
+
+// insert new employee
+
+// update employee
+
+// update assignment
+
+// delete employee
+
+// delete assignment
+
+app.listen(PORT, () => {
+	console.log(`Server running on ${PORT}...`);
 });
